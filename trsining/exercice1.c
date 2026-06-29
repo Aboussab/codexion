cec879720c3391b63629947437ee4e7fca081518
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-06-30 00:34:04 +0100

    faaaaaaahhhhhhh

--- trsining/exercice1.c
+++ trsining/exercice1.c
@@ -1,57 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include <pthread.h>
 
 long long counter = 0;
-typedef struct threa
-{
-    pthread_mutex_t mutex1;
-    pthread_t   thread;
-    int tid_num;
-}threa;
+pthread_mutex_t mutex1;
 
 
 void* hello_thread(void* i)
 {
-    printf("thread %i is working",(threa *)i->tid_num)
-    
+    printf("thread %i is working\n",*(int *)i);
+    return NULL;
 }
 
 
 int main()
 {
-    threa *tit1;
-    threa *tit2;
-    threa *tit3;
+    pthread_t   thread1;
+    pthread_t   thread2;
+    pthread_t   thread3;
 
-    tit1 = (threa*) malloc(sizeof(threa));
-    if (!tit1)
-    {
-        return 0;
-    }
-    tit2 = (threa*) malloc(sizeof(threa));
-    if (!tit2)
-    {
-        free(tit1);
-        return 0;
-    }
-    tit3 = (threa*) malloc(sizeof(threa));
-    if (!tit3)
-    {
-        free(tit1);
-        free(tit2);
-        return 0;
-    }
 
-    pthread_create(&(tit1->thread),NULL,hello_thread,tit1);
-    pthread_create(&(tit2->thread),NULL,hello_thread,tit2);
-    pthread_create(&(tit3->thread),NULL,hello_thread,tit3);
+    int start = gettimeofday();
+    pthread_mutex_init(&mutex1, NULL);
+    int i1 = 1;
+    int i2 = 2;
+    int i3 = 3;
+    pthread_mutex_lock(&mutex1);
 
-    pthread_join(tit1->thread, NULL);
-    pthread_join(tit2->thread, NULL);
-    pthread_join(tit3->thread, NULL);
-    
-    printf("%lld \n", counter);
+    pthread_create(&thread1,NULL,hello_thread,&i1);
+    pthread_create(&thread2,NULL,hello_thread,&i2);
+    pthread_create(&thread3,NULL,hello_thread,&i3);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_mutex_unlock(&mutex1);
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    usleep(20000);
+
+    // ... run your code or usleep() here ...
+
+    gettimeofday(&end, NULL);
+    // Convert everything to milliseconds
+    long long end_time = (end.tv_sec * 1000LL) + (end.tv_usec / 1000);
+
+    printf("Elapsed time: %lld ms\n", end_time - start_time);   
     return 0;
 }
