5cbd3130e6ae0deb8fbd563dadd6de4f027e82ed
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-07-10 02:29:04 +0100

    exercice 8

--- trsining/exercice1.c
+++ trsining/exercice1.c
@@ -5,25 +5,42 @@
 #include <pthread.h>
 
 long long counter = 0;
-pthread_mutex_t mutex1;
+pthread_mutex_t mutex;
+pthread_cond_t  conc;
 
-
-void* hello_thread(void* i)
+void* MAIN_SAY(void* i)
 {
-    printf("thread %i is working\n",*(int *)i);
+    // usleep(3000000);
+    pthread_mutex_lock(&mutex);
+    printf("Wake up!");
+    counter++;
+    pthread_mutex_unlock(&mutex);
+    pthread_cond_signal(&conc);
+    return NULL;
+}
+void* WORKER_SAY(void* i)
+{
+    pthread_mutex_lock(&mutex);
+    printf("Worker: I'm waiting...");
+    while (counter == 0)
+        pthread_cond_wait(&conc, &mutex);
+    printf("I'm awake!");
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
 
-
 int main()
 {
-    struct timeval tv;
-    
-    gettimeofday(&tv, NULL);
-    int start = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
-    usleep(2000000);
-    gettimeofday(&tv, NULL);
-    int end = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
-    printf("Elapsed time: %d ms\n", end - start);
+    pthread_t   main;
+    pthread_t   worker;
+
+    pthread_mutex_init(&mutex,NULL);
+    pthread_cond_init(&conc, &mutex);
+    pthread_create(&worker, NULL, WORKER_SAY, NULL);
+    pthread_create(&main, NULL, MAIN_SAY, NULL);
+    pthread_join(&worker, NULL);
+    pthread_join(&main,NULL);
+     pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&conc);
     return 0;
 }
