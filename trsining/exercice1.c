e72e9b7473073a9e6ee7ad8ead3a96e6382c083e
aboussab <aminebossaboune@gmail.com>
2026-07-10 03:48:28 +0100

    ex9

--- trsining/exercice1.c
+++ trsining/exercice1.c
@@ -8,39 +8,28 @@ long long counter = 0;
 pthread_mutex_t mutex;
 pthread_cond_t  conc;
 
-void* MAIN_SAY(void* i)
-{
-    // usleep(3000000);
-    pthread_mutex_lock(&mutex);
-    printf("Wake up!");
-    counter++;
-    pthread_mutex_unlock(&mutex);
-    pthread_cond_signal(&conc);
-    return NULL;
-}
 void* WORKER_SAY(void* i)
 {
-    pthread_mutex_lock(&mutex);
-    printf("Worker: I'm waiting...");
-    while (counter == 0)
-        pthread_cond_wait(&conc, &mutex);
-    printf("I'm awake!");
-    pthread_mutex_unlock(&mutex);
-    return NULL;
+   
+}
+void* SIGNALER_SAY(void* i)
+{
+    
 }
 
 int main()
 {
-    pthread_t   main;
-    pthread_t   worker;
-
-    pthread_mutex_init(&mutex,NULL);
-    pthread_cond_init(&conc, &mutex);
+    pthread_t worker;
+    pthread_t signaler;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&conc, NULL);
     pthread_create(&worker, NULL, WORKER_SAY, NULL);
-    pthread_create(&main, NULL, MAIN_SAY, NULL);
-    pthread_join(&worker, NULL);
-    pthread_join(&main,NULL);
-     pthread_mutex_destroy(&mutex);
+    pthread_create(&signaler, NULL, SIGNALER_SAY, NULL);
+
+    pthread_join(worker, NULL);
+    pthread_join(signaler, NULL);
+    pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&conc);
+
     return 0;
 }
